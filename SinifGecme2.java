97c1ea17e35fc4ab00cdd498e3326e71bfe4e2b3
üzeyir <105324885+Uzeyir-Muhammed-Yavuz@users.noreply.github.com>
2022-05-20 22:00:52 +0300

    Add files via upload

---
+++ SinifGecme2.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class SinifGecme2 {
+    public static void main(String[] args) {
+
+        double matematik, fizik, türkce, kimya, muzik, ortalama =0, sonuc ;
+        int bDers = 0;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Matematik notunuz :");
+        matematik = inp.nextDouble();
+
+        if (matematik >= 0 && matematik <= 100) {
+            ortalama = + matematik;
+            bDers =+1;
+
+        } else {
+            System.out.println("Notunuz 0 ve 100 arasında olmadığından değerlendirmeye alınmamıştır !");
+
+        }
+        System.out.print("Fizik notunuz :");
+        fizik = inp.nextDouble();
+
+        if (fizik >= 0 && fizik <= 100) {
+            ortalama = +fizik;
+            bDers= +1;
+
+        } else {
+            System.out.println("Notunuz 0 ve 100 arasında olmadığından değerlendirmeye alınmamıştır !");
+        }
+        System.out.print("Türkçe notunuz :");
+        türkce = inp.nextDouble();
+
+        if (türkce >= 0 && türkce <= 100) {
+            ortalama = +türkce;
+            bDers = +1;
+
+        } else {
+            System.out.println("Notunuz 0 ve 100 arasında olmadığından değerlendirmeye alınmamıştır !");
+        }
+        System.out.print("Kimya notunuz :");
+        kimya = inp.nextDouble();
+
+        if (kimya >= 0 && kimya <= 100) {
+            ortalama = +kimya;
+            bDers = +1;
+
+        } else {
+            System.out.println("Notunuz 0 ve 100 arasında olmadığından değerlendirmeye alınmamıştır !");
+        }
+        System.out.print("Müzik notunuz :");
+        muzik = inp.nextDouble();
+
+        if (muzik >= 0 && muzik <= 100) {
+            ortalama = +muzik;
+            bDers = +1;
+        } else {
+
+            System.out.println("Notunuz 0 ve 100 arasında olmadığından değerlendirmeye alınmamıştır !");
+        }
+
+        sonuc = ortalama / bDers;
+
+        if (sonuc >= 55) {
+            System.out.println("Sınıfı geçtiniz :" + sonuc);
+        } else {
+
+            System.out.println("Sınıf tekrarı :" + sonuc);
+        }
+    }
+
+
+}
+
+
